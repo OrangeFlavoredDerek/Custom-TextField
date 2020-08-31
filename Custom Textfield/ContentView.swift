e114ca4bdd3d2020e2dc9f8b573a79a91a3a1119
Derek <chenjunqi07@icloud.com>
2020-08-31 09:30:19 +0800

    Initial Commit

---
+++ Custom Textfield/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Custom Textfield
+//
+//  Created by Derek Chan on 2020/8/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Custom Textfield/Custom_TextfieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_TextfieldApp.swift
+//  Custom Textfield
+//
+//  Created by Derek Chan on 2020/8/31.
+//
+
+import SwiftUI
+
+@main
+struct Custom_TextfieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
